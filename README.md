a11df4ec56391bab5d9f919ce4bf7e3a4a0d792f
Scott Doxey <neogeek0101@gmail.com>
2016-10-17 07:47:02 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+# doxdox-plugin-markdown
+
+> Markdown template plugin for doxdox.
+
+## Install
+
+```bash
+$ npm install doxdox doxdox-plugin-markdown --save-dev
+```
+
+## Usage
+
+```bash
+$ doxdox lib/ --layout markdown --output DOCUMENTATION.md
+```
---
+++ index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const Handlebars = require('handlebars');
+
+const plugin = data => new Promise((resolve, reject) => {
+
+    fs.readFile(path.join(__dirname, './template.hbs'), 'utf8', (err, contents) => {
+
+        if (err) {
+
+            return reject(err);
+
+        }
+
+        const template = Handlebars.compile(contents);
+
+        return resolve(template(data));
+
+    });
+
+});
+
+module.exports = plugin;
