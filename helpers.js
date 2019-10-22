5c4538d1a241d2f5a85f9b8a9006c2157660c2b7
Taylor Dawson <taylorjdawson@gmail.com>
2019-10-22 14:34:42 -0700

    update formating

--- helpers.js
+++ helpers.js
@@ -2,3 +2,5 @@ const Handlebars = require('handlebars');
 
 Handlebars.registerHelper('replaceNewLinesWithBR', block =>
     block.replace(/[\r\n]+/gu, '<br>'));
+Handlebars.registerHelper('formatDescription', descript => descript.replace(/-/g, ''));
+Handlebars.registerHelper('isType', (type, _type) => type === _type);
